3e6829761ee13d823ba60941379617377a54b06c
maze <infimo000@gmail.com>
2024-11-24 22:40:59 +1000

    mod back

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, jsonify, request, send_from_directory
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+from bson import ObjectId
+import os
+
+# Instantiation
+app = Flask(__name__, static_folder="build")  # La carpeta 'build' se servirá como estática
+app.config['MONGO_URI'] = 'mongodb://localhost/pythonreact'
+mongo = PyMongo(app)
+
+# Settings
+CORS(app)
+
+# Database
+db = mongo.db.pythonreact
+
+# Routes
+@app.route('/users', methods=['POST'])
+def createUser():
+    print(request.json)
+    result = db.insert_one({
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'password': request.json['password']
+    })
+    return jsonify(str(result.inserted_id))
+
+@app.route('/users', methods=['GET'])
+def getUsers():
+    users = []
+    for doc in db.find():
+        users.append({
+            '_id': str(doc['_id']),
+            'name': doc['name'],
+            'email': doc['email'],
+            'password': doc['password']
+        })
+    return jsonify(users)
+
+@app.route('/users/<id>', methods=['GET'])
+def getUser(id):
+    user = db.find_one({'_id': ObjectId(id)})
+    return jsonify({
+        '_id': str(user['_id']),
+        'name': user['name'],
+        'email': user['email'],
+        'password': user['password']
+    })
+
+@app.route('/users/<id>', methods=['DELETE'])
+def deleteUser(id):
+    db.delete_one({'_id': ObjectId(id)})
+    return jsonify({'message': 'User Deleted'})
+
+@app.route('/users/<id>', methods=['PUT'])
+def updateUser(id):
+    print(request.json)
+    db.update_one({'_id': ObjectId(id)}, {"$set": {
+        'name': request.json['name'],
+        'email': request.json['email'],
+        'password': request.json['password']
+    }})
+    return jsonify({'message': 'User Updated'})
+
+# Serve React Frontend
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve_frontend(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, "index.html")
+
+# Main entry point
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+
---
+++ build/precache-manifest.051cb95fa81633a98dcbc27c49523989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "078274f074d0662896c9b12bff3c44d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "450fde0cd7e9928a129e",
+    "url": "/static/css/2.ef1efacd.chunk.css"
+  },
+  {
+    "revision": "f4a02876cb898c515a5f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "450fde0cd7e9928a129e",
+    "url": "/static/js/2.2815487f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2815487f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a02876cb898c515a5f",
+    "url": "/static/js/main.5fefe1d6.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
